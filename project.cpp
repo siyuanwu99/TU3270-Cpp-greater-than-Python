ebee47ba535fa04663ceebbc83dc192dfa3c3e4d
lxj595595 <xinjieliu98@163.com>
2022-01-28 00:53:02 +0100

    finish matrix class (small bug in operator*)

--- project.cpp
+++ project.cpp
@@ -79,6 +79,15 @@ class Vector {
     return *this;
   }
 
+  /**change the value of all the entry as a constant**/
+  const Vector<T>& operator=(const int constant) {
+      for (int i = 0; i < this->n; ++i) {
+        T value = constant;
+        data[i] = value;
+      }
+    return *this;
+  }
+
   /** move assignment **/
   const Vector<T>& operator=(Vector<T>&& other) {
     if (this != &other) {
@@ -142,7 +151,7 @@ class Vector {
   friend Vector<typename std::common_type<V, U>::type>& operator*(
       const V& scalar, const Vector<U>& vec);
 
-  int len(void) { return this->n; }
+  int len(void) const { return this->n; }
 };
 
 template <typename V, typename U>
@@ -176,70 +185,126 @@ T norm(const Vector<T>& vec) {
   return sqrt(sum);
 }
 
+/**
+ * @brief Matrix Class
+ *
+ * @tparam T
+ */
 template <typename T>
 class Matrix {
+
  private:
   int r, c;
   std::map<std::pair<int, int>, T> data;
 
  public:
+  // constructor
   Matrix(int rows, int cols) : r(rows), c(cols){};
+  
+  // destructor
   ~Matrix() {
     r = 0;
     c = 0;
     data.clear();
   }
 
+  // getter
+  int row(void)const{return this->r;}
+  int col(void)const{return this->c;}
+
+  // iterator
+  auto begin()const{return this->data.begin();}
+  auto end()const{return this->data.end();}
+  auto cbegin()const{return this->data.cbegin();}
+  auto cend()const{return this->data.cend();}
+
+  // index operator
   T& operator[](const std::pair<int, int>& ij) {
-    auto it = data.find(ij);
-    if (it != data.end()) {
-      return data.at(ij);
-    } else {
-      data.insert(ij, 0);
-      return data.at(ij);
+    auto it = data.begin();
+    for(;it != data.end(); ++it){
+        int i = it->first.first;
+        int j = it->first.second;
+        if(i==ij.first && j==ij.second){
+            return it->second;
+        }
     }
+    // if the queried entry does not exist
+    data.insert(std::make_pair(ij, 0));
+    return data.at(ij);
   }
 
-  T& operator()(const std::pair<int, int>& ij) const {
-    auto it = data.find(ij);
-    if (it != data.end()) {
-      return data.at(ij);
-    } else {
-      // return std::static_cast<T>(0);
-      return 0;
+  // index operator for constant reference
+  const T& operator()(const std::pair<int, int>& ij) const{
+    auto it = data.begin();
+    for(;it != data.end(); ++it){
+        int i = it->first.first;
+        int j = it->first.second;
+        if(i==ij.first && j==ij.second){
+            return it->second;
+        }
     }
+    // if the queried entry does not exist
+    throw "The queried entry does not exist!";
   }
 
-  // template <typename V, typename U>
-  // Vector<typename std::common_type<V, U>::type> operator*(
-  //     const Matrix<T>& lhs, const Vector<U>& rhs) {
-  //   std::cout << std::endl;
-  // }
 };
+
+//vector matrix multiplication
+template <typename V, typename U>
+Vector<typename std::common_type<V, U>::type> operator*(
+const Matrix<V>& lhs, const Vector<U>& rhs){
+    if(rhs.len() != lhs.col()){
+        throw "Incomatible dimensions of the vector and the matrix!";
+    }
+    Vector<typename std::common_type<V, U>::type> new_vec(lhs.row());
+    new_vec = 0;
+    for(auto it = lhs.cbegin(); it != lhs.cend(); ++it){
+        int i = it->first.first;
+        int j = it->first.second;
+        new_vec[i] += it->second * rhs[j]; 
+    }
+    return new_vec;
+}
+
 
 template <typename T>
 int bicgstab(const Matrix<T>& A, const Vector<T>& b, Vector<T>& x,
              T tol = (T)1e-8, int maxiter = 100) {
   // Your implementation of the bicgstab function starts here
+  return 0;
 }
 
 template <typename T>
 void heun(const Vector<std::function<T(Vector<T> const&, T)> >& f, Vector<T>& y,
           T h, T& t){
     // Your implementation of the heun function starts here
-};
+}
 
 template <typename T>
 class SimplestWalker {
   // Your implementation of the simplest walker class starts here
 };
 
+
 int main(int argc, char* argv[]) {
   // Your testing of the simplest walker class starts here
-  // test vector
-  Vector<double> x({1.0, 1.1, 1.2}), y({2, 3, 4}), z({1.0f, 2.0f, 3.0f}), w;
-  w = y * 3;
-  w = 2 * x;
-  w = 2 * x + y * 3;
+  // test Matrix
+  Matrix<double> M(10, 20), M1(10, 3);
+  Vector<double> x({1.0, 1.1, 1.2}), y({2, 3, 4}), z({1.0f, 2.0f, 3.0f});
+
+  try{
+    M[{1,9}] = 1.0; // set value at row 0, column 0 to 1.0
+    std::cout << M[{1, 9}] << std::endl;
+    std::cout << M[{0, 0}] << std::endl;
+    std::cout << M({1, 9}) << std::endl;
+    std::cout << typeid(M.row()).name() << ' ' << M.col() << std::endl;
+    Vector<double>v2(M1*x);
+    //std::cout << M*x << std::endl;
+    std::cout << x[2] << std::endl;
+    std::cout << M({1, 1}) << std::endl;
+  }
+  catch(const char* msg){
+    std::cerr << msg << std::endl;
+  }
   return 0;
 }
